377f6787017303e8fcad0b5b7d633a3a14f8f41b
Subhash Nayak <64570322+subhash131@users.noreply.github.com>
2023-11-14 22:17:36 +0530

    Update CustomerApplication.java

--- src/main/java/com/example/demo/CustomerApplication.java
+++ src/main/java/com/example/demo/CustomerApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class CustomerApplication {
+
+	@GetMapping("/")
+	public String getMessage() {
+		return "Hi, Im Alive";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
